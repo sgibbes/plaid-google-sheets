727df68d7994951996b1f7adaf7dc0a29dcb9f2d
Sam Gibbes <samagibb@gmail.com>
2025-05-06 09:17:03 -0400

    sync code

--- bankFunctions.js
+++ bankFunctions.js
@@ -1,31 +1,4 @@
 /** @format */
-function createDataInSheet() {
-  const sheet = spreadsheet.insertSheet(); // Creates a sheet with a default name
-  sheet.setName(newSheetName); // names new sheet
-
-  sheet.clearContents();
-  sheet.appendRow(["Date", "Transaction Description", "Transaction Amount"]);
-  txAdjusted.forEach((tx) => {
-    sheet.appendRow([tx.date, tx.name, tx.amount]);
-  });
-
-  sheet.getRange(1, 6).setValue("Clear Filter");
-  sheet.getRange(1, 7).insertCheckboxes();
-
-  sheet.getRange(2, 6).setValue("Run Categories");
-  sheet.getRange(2, 7).insertCheckboxes();
-
-  sheet.getRange(3, 6).setValue("Create Summary Table");
-  sheet.getRange(3, 7).insertCheckboxes();
-
-  sheet.getRange(4, 6).setValue("Re-Download Data");
-  sheet.getRange(4, 7).insertCheckboxes();
-
-  sheet.getRange(5, 6).setValue("Create Charts");
-  sheet.getRange(5, 7).insertCheckboxes();
-
-  sheet.autoResizeColumns(1, 10);
-}
 
 function getRealTransactions(userMonth = null, userYear = null, append = false) {
   Logger.log(append);
@@ -113,15 +86,15 @@ function getRealTransactions(userMonth = null, userYear = null, append = false) 
   });
 
   const newSheetName = `${month}-${year}`;
-  Logger.log({ newSheetName });
+Logger.log({newSheetName})
 
   const spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
   const sheetToCheck = spreadsheet.getSheetByName(newSheetName);
-  Logger.log({ sheetToCheck });
-  Logger.log({ append });
+  Logger.log({sheetToCheck})
+  Logger.log({append})
 
   if (sheetToCheck && !append) {
-    Logger.log("here");
+    Logger.log('here') 
 
     const response = Browser.msgBox(
       `Sheet ${newSheetName} already exists. Overwrite sheet?`, // message
@@ -129,6 +102,7 @@ function getRealTransactions(userMonth = null, userYear = null, append = false) 
     );
 
     if (response === "ok") {
+
       spreadsheet.deleteSheet(sheetToCheck);
 
       const sheet = spreadsheet.insertSheet(); // Creates a sheet with a default name
@@ -163,28 +137,29 @@ function getRealTransactions(userMonth = null, userYear = null, append = false) 
 
   // the sheet does not already exist
   if (!sheetToCheck) {
-    const sheet = spreadsheet.insertSheet(); // Creates a sheet with a default name
-    sheet.setName(newSheetName); // names new sheet
+ const sheet = spreadsheet.insertSheet(); // Creates a sheet with a default name
+      sheet.setName(newSheetName); // names new sheet
 
-    sheet.clearContents();
-    sheet.appendRow(["Date", "Transaction Description", "Transaction Amount"]);
-    txAdjusted.forEach((tx) => {
-      sheet.appendRow([tx.date, tx.name, tx.amount]);
-    });
+      sheet.clearContents();
+      sheet.appendRow(["Date", "Transaction Description", "Transaction Amount"]);
+      txAdjusted.forEach((tx) => {
+        sheet.appendRow([tx.date, tx.name, tx.amount]);
+      });
 
-    sheet.getRange(1, 6).setValue("Clear Filter");
-    sheet.getRange(1, 7).insertCheckboxes();
+      sheet.getRange(1, 6).setValue("Clear Filter");
+      sheet.getRange(1, 7).insertCheckboxes();
 
-    sheet.getRange(2, 6).setValue("Run Categories");
-    sheet.getRange(2, 7).insertCheckboxes();
+      sheet.getRange(2, 6).setValue("Run Categories");
+      sheet.getRange(2, 7).insertCheckboxes();
 
-    sheet.getRange(3, 6).setValue("Create Summary Table");
-    sheet.getRange(3, 7).insertCheckboxes();
+      sheet.getRange(3, 6).setValue("Create Summary Table");
+      sheet.getRange(3, 7).insertCheckboxes();
 
-    sheet.getRange(4, 6).setValue("Re-Download Data");
-    sheet.getRange(4, 7).insertCheckboxes();
+      sheet.getRange(4, 6).setValue("Re-Download Data");
+      sheet.getRange(4, 7).insertCheckboxes();
 
-    sheet.autoResizeColumns(1, 10);
+
+      sheet.autoResizeColumns(1, 10);
   }
 
   if (append) {
